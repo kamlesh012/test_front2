11b679c3f4907c5ac6dac2880a685ce83c855538
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-09-10 11:43:16 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@
 //   process.env.NODE_ENV === 'development'
 //     ? 'http://localhost:5000'
 //     : 'https://testback-qsav.onrender.com/';
-export const BASE_URL = 'https://testback-qsav.onrender.com';
+export const BASE_URL = 'https://vegimart-backend-st1x.onrender.com/';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
